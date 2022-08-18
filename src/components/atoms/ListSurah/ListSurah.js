0a643f675be8cd9bdc8192db4580a0e857731a5c
Jajang Rohmatulloh <jajang.rohmatulloh02@gmail.com>
2022-08-18 10:11:46 +0700

    add

---
+++ src/components/atoms/ListSurah/ListSurah.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const ListSurah = (props) => {
+    return (
+        <div className="rows-surah" onClick={ () => props.goDetail(props.data.number)}>
+            {props.data.number}: {props.data.englishName} <span>{props.data.name}</span>
+        </div>
+    )
+}
+export default ListSurah;
