aeed2e6497d51ce5454ba0d8f3b901b4789186e9
Jajang Rohmatulloh <jajang.rohmatulloh02@gmail.com>
2022-08-21 13:34:02 +0700

    convert

--- src/components/atoms/ListAyat/ListAyat.js
+++ src/components/atoms/ListAyat/ListAyat.js
@@ -1,17 +1,26 @@
 import React from 'react';
 
 const ListAyat = (props) => {
+    function toFarsiNumber(n) {
+        const farsiDigits = ['۰', '۱', '۲', '۳', '٤', '۵', '٦', '۷', '۸', '۹'];
+    
+        return n
+            .toString()
+            .replace(/\d/g, x => farsiDigits[x]); 
+    }
     return ( 
         <>
             {
                 props.ayat.number !== 1 &&
                 props.ayat.number !== 1236 &&
                 props.ayat.numberInSurah === 1 &&
-                <div>{props.ayat.text.substring(0, 38)}</div>
+                <div className="bismillah">
+                    <span>{props.ayat.text.substring(0, 38)}</span>
+                </div>
             }
         <div className="rows-ayat">
-            <span className="ayat">{props.ayat.numberInSurah}</span>
-            <span className="quran">
+            <span className="numberAyat">{toFarsiNumber(props.ayat.numberInSurah)}</span>
+            <span className="ayat">
                 {
                     props.ayat.number !== 1 &&
                     props.ayat.number !== 1236 &&
