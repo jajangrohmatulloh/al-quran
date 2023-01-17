c1d2d09fb6a91099f02ec5978b492733aa69f098
Jajang Rohmatulloh <jajang.rohmatulloh02@gmail.com>
2023-01-17 17:13:22 +0700

    add assets

--- src/containers/pages/App/App.js
+++ src/containers/pages/App/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import './App.css';
-import Surah from '../Surah/Surah';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Ayat from '../Ayat/Ayat';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+import Surah from "../Surah/Surah";
+import Ayat from "../Ayat/Ayat";
 
-const App = () => {
+function App() {
   return (
     <BrowserRouter>
       <TransitionGroup>
-        <CSSTransition
-          classNames="fade"
-          timeout={1000}>
+        <CSSTransition classNames="fade" timeout={1000}>
           <Switch>
             <Route path="/" exact component={Surah} />
             <Route path="/surah/:number" component={Ayat} />
           </Switch>
         </CSSTransition>
       </TransitionGroup>
-
     </BrowserRouter>
   );
 }
--- src/containers/pages/Surah/Surah.js
+++ src/containers/pages/Surah/Surah.js
@@ -2,52 +2,51 @@ import React, { Component } from 'react';
 import ListSurah from '../../../components/atoms/ListSurah/ListSurah';
 import Loader from '../../../components/atoms/Loader/Loader';
 import alquran from '../../../assets/al-quran.png';
+
 class Surah extends Component {
-    state = {
-        lists: [],
-        load: true
-    }
-    componentDidMount() {
-        fetch(`http://api.alquran.cloud/v1/surah`)
-          .then(res => res.json())
-          .then(res => {
-              this.setState({
-                  lists: res.data,
-                  load: false
-                })
-          })
-    }
-    handleClick = (id) => {
-        this.props.history.push(`/surah/${id}`)
-    }
-    render() {
-        return (
-            <>
-            <header 
-            style={
-                {
-                    display: 'flex',
-                    justifyContent: 'center'
-                }
-                }>
-                <div>
-                Surah
-                </div>               
-            </header>
-            
-            <div className="container">
-            <div class="image">
-                <img src={alquran} alt=""/>
-            </div>
-                {this.state.load && <Loader />}
-                {
-                this.state.lists.map(list => 
-                    <ListSurah data={list} goDetail={this.handleClick}/>
-                    )
-                }
-            </div>
-            </>
-        )
-    }
+  state = {
+    lists: [],
+    load: true,
+  };
+
+  componentDidMount() {
+    fetch('http://api.alquran.cloud/v1/surah')
+      .then((res) => res.json())
+      .then((res) => {
+        this.setState({
+          lists: res.data,
+          load: false,
+        });
+      });
+  }
+
+  handleClick = (id) => {
+    this.props.history.push(`/surah/${id}`);
+  };
+
+  render() {
+    return (
+      <>
+        <header
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+          }}
+        >
+          <div>Surah</div>
+        </header>
+
+        <div className="container">
+          <div className="image">
+            <img src={alquran} alt="" />
+          </div>
+          {this.state.load && <Loader />}
+          {this.state.lists.map((list) => (
+            <ListSurah data={list} goDetail={this.handleClick} />
+          ))}
+        </div>
+      </>
+    );
+  }
 }
 export default Surah;
